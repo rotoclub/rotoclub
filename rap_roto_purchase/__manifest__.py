f8580b88af09c2971486c8871e3562e7363145b4
rubengarpra <rurugar275@gmail.com>
2023-05-05 14:53:18 +0200

    added new cron for purchases and modified journeis filter

--- rap_roto_purchase/__manifest__.py
+++ rap_roto_purchase/__manifest__.py
@@ -9,7 +9,7 @@
     'category': 'Uncategorized',
     'license': 'LGPL-3',
 
-    'version': '15.0.1.1.0',
+    'version': '15.0.1.1.1',
 
     'depends': [
         'base',
@@ -20,6 +20,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/roto_view.xml',
+        'data/crons.xml',
     ],
     'application': False,
 }
